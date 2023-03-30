f41887e25fb35a3adcac2ace83534ac196ba54c8
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-30 13:35:10 +0200

    remove: unnecesary pages

--- src/views/pages/ActivePage/ActivePage.tsx
+++
@@ -1,5 +0,0 @@
-import WishlistComponent from 'views/components/WishlistComponent';
-
-export default function ActivePage() {
-  return <WishlistComponent status={'active'} />;
-}
--- src/views/pages/CompletedPage/CompletedPage.tsx
+++
@@ -1,5 +0,0 @@
-import WishlistComponent from 'views/components/WishlistComponent';
-
-export default function CompletedPage() {
-  return <WishlistComponent status={'completed'} />;
-}
