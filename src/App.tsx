54d3c31aa50e60720214e9dd4ca9b0ebcd7f0a5f
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-30 20:43:11 +0200

    Add Auth0 authentication provider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { RouterProvider } from 'react-router-dom';
-import router from './routes/router';
+import { Auth0Provider } from '@auth0/auth0-react';
 import WishlistProvider from 'context/wishlist/WishlistProvider';
+import router from './routes/router';
+import { isLocalhost } from 'utils/isLocalhost';
 import { Toaster } from 'react-hot-toast';
 
 export default function App() {
   return (
-    <WishlistProvider>
-      <Toaster />
-      <RouterProvider
-        router={router}
-        fallbackElement={<></>}
-      />
-    </WishlistProvider>
+    <Auth0Provider
+      domain={`${process.env.REACT_APP_AUTH0_DOMAIN}`}
+      clientId={
+        isLocalhost
+          ? `${process.env.REACT_APP_AUTH0_CLIENT_ID_DEVELOPMENT}`
+          : `${process.env.REACT_APP_AUTH0_CLIENT_ID_PRODUCTION}`
+      }
+      authorizationParams={{ redirect_uri: window.location.origin + '/' }}
+    >
+      <WishlistProvider>
+        <Toaster />
+        <RouterProvider
+          router={router}
+          fallbackElement={<></>}
+        />
+      </WishlistProvider>
+    </Auth0Provider>
   );
 }
