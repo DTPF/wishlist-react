1806e3191aac9c1d0c4d33e9b01d0aa43091a13f
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-30 13:25:35 +0200

    feat(footer): create main footer component

---
+++ src/views/components/layout/footerWishlist/FooterWishlist.tsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import WishlistContext from 'context/wishlist/WishlistContext';
+import RemoveWishlist from 'views/components/WishlistComponent/RemoveWishlist'
+import './footerWishlist.scss'
+
+export default function FooterWishlist() {
+	const { wishlist } = useContext(WishlistContext);
+
+	return (
+		<footer className='footer-wish-list'>
+			<div className='footer-wish-list__authentication'>
+				Identifícate para tener tu propia lista
+			</div>
+			<div className='footer-wish-list__remove-list'>
+				{wishlist.length > 0 && <RemoveWishlist />}
+			</div>
+		</footer>
+	)
+}
