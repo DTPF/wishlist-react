ba6dec6563ae34efa0931202af0b66f25ab8a504
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-30 13:23:58 +0200

    remove: pages routes from router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,21 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 // Pages
 import HomePage from '../views/pages/HomePage/HomePage';
-import ActivePage from '../views/pages/ActivePage';
-import CompletedPage from '../views/pages/CompletedPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
-  },
-  {
-    path: "active",
-    element: <ActivePage />,
-  },
-  {
-    path: "completed",
-    element: <CompletedPage />
+    children: [
+      {
+        path: ":isCompleted",
+        element: <HomePage />,
+      },
+    ]
   },
   {
     path: "*",
