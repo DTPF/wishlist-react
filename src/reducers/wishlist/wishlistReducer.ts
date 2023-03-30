7f3bbc86d985358183aebfbe61ca110ca0f10135
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-30 13:22:28 +0200

    Update wishlist reducer to include initializing wishlist.

--- src/reducers/wishlist/wishlistReducer.ts
+++ src/reducers/wishlist/wishlistReducer.ts
@@ -4,6 +4,12 @@ export default function wishlistReducer(state: any, action: any) {
 	const { type, payload } = action;
 
 	switch (type) {
+		case WishlistTypes.INIT_WISHLIST:
+			return {
+				...state,
+				wishlist: payload.wishlistItems
+			}
+
 		case WishlistTypes.ADD_WISHLIST:
 			return {
 				...state,
