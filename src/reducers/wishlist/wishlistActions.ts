40a5ac62b692f3a68643686fab68767a04a36e24
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-03-30 13:22:13 +0200

    Add functionality to add and initialize wishlist items.

--- src/reducers/wishlist/wishlistActions.ts
+++ src/reducers/wishlist/wishlistActions.ts
@@ -1,2 +1,27 @@
-export const initCurrentTracklistAction = function (dispatch: any) {
+import { getWishlistApi, postWishlistApi } from "api/wishlist";
+import * as WishlistTypes from "reducers/wishlist/wishlistTypes";
+import toast from 'react-hot-toast';
+
+export const initWishlistAction = function (dispatch: any) {
+	getWishlistApi().then((res: any) => {
+		if (res.status === 200) {
+			dispatch({ type: WishlistTypes.INIT_WISHLIST, payload: res });
+		} else {
+			toast.error(res.message);
+		}
+	})
+}
+
+export const addWishlistAction = function (dispatch: any, item: any, tracklistState: any) {
+	postWishlistApi(item).then((res: any) => {
+		if (res.status === 200) {
+			let newObj = [...tracklistState.wishlist];
+			newObj.push(res.wishlistItem);
+
+			dispatch({ type: WishlistTypes.ADD_WISHLIST, payload: newObj });
+			toast.success(res.message);
+		} else {
+			toast.error(res.message);
+		}
+	})
 }
