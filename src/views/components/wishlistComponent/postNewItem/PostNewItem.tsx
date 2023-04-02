94ba0469e831b52e794e530d4601310e553bf30f
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-02 18:20:00 +0200

    change(postNewWishlistItem)
    Update PostNewItem component to account for adding new wishlist items
    with unique IDs and provide feedback with toast messages

--- src/views/components/wishlistComponent/postNewItem/PostNewItem.tsx
+++ src/views/components/wishlistComponent/postNewItem/PostNewItem.tsx
@@ -1,36 +1,39 @@
-import { useState, useRef, useContext } from 'react';
-import WishlistContext from 'context/wishlist/WishlistContext';
-import { RiSendPlaneFill } from 'react-icons/ri';
-import './postNewItem.scss';
+import { useState, useContext } from 'react'
+import WishlistContext from 'context/wishlist/WishlistContext'
+import { RiSendPlaneFill } from 'react-icons/ri'
+import './postNewItem.scss'
+import { v4 as uuidv4 } from 'uuid'
+import { toast } from 'react-hot-toast'
 
 export default function PostNewItem() {
-  const { addWishlist } = useContext(WishlistContext)
+  const { addNewWishlistItem } = useContext(WishlistContext)
   const [inputs, setInputs] = useState({
-    title: undefined,
-  });
-  const form: any = useRef();
+    wishlistItemId: uuidv4(),
+    title: '',
+  })
 
   const handleSubmit = (e: any) => {
-    e.preventDefault();
-
-    if (!inputs.title) {
-      return alert('Escribe algo!');
-    }
-    form.current.reset();
-    addWishlist(inputs)
+    e.preventDefault()
+    if (!inputs.title) return toast.error('¡Escribe algo!')
+    addNewWishlistItem(inputs)    
+    setInputs({
+      ...inputs,
+      title: '',
+    })
   }
 
   const handleChangeForm = (e: any) => {
-    setInputs({ ...inputs, [e.target.name]: e.target.value });
-  };
+    setInputs({ ...inputs, [e.target.name]: e.target.value })
+  }
 
   return (
     <article className='post-new-item-form'>
-      <form className='post-new-item-form__form' ref={form} onSubmit={(e) => handleSubmit(e)}>
+      <form className='post-new-item-form__form' onSubmit={(e) => handleSubmit(e)}>
         <input
           name='title'
           type='text'
           placeholder='Añade algo nuevo'
+          value={inputs.title}
           onChange={(e) => handleChangeForm(e)}
         />
         <button type='submit'><RiSendPlaneFill /></button>
