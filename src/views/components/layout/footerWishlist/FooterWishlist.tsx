c51003f28ad1ab6c89c4891ef088432691b7e4a8
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-02 17:48:32 +0200

    }Add authentication logic and logout button to FooterWishlist component.

--- src/views/components/layout/footerWishlist/FooterWishlist.tsx
+++ src/views/components/layout/footerWishlist/FooterWishlist.tsx
@@ -2,17 +2,31 @@ import { useContext } from 'react';
 import WishlistContext from 'context/wishlist/WishlistContext';
 import RemoveWishlist from 'views/components/wishlistComponent/removeWishlist'
 import './footerWishlist.scss'
+import { useAuth0 } from '@auth0/auth0-react';
+import { IoMdExit } from 'react-icons/io';
 
 export default function FooterWishlist() {
-	const { wishlist } = useContext(WishlistContext);
+	const { currentWishlist } = useContext(WishlistContext);
+	const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
 
 	return (
 		<footer className='footer-wish-list'>
 			<div className='footer-wish-list__authentication'>
-				Identifícate para tener tu propia lista
+				{!isAuthenticated && (
+					<p>
+						<span onClick={() => loginWithRedirect()}>
+							Identifícate para tener tu propia lista
+						</span>
+					</p>
+				)}
 			</div>
 			<div className='footer-wish-list__remove-list'>
-				{wishlist.length > 0 && <RemoveWishlist />}
+				{currentWishlist?.wishlistItems?.length > 0 && <RemoveWishlist />}
+				{isAuthenticated && (
+					<button onClick={() => logout()}>
+						<IoMdExit />
+					</button>
+				)}
 			</div>
 		</footer>
 	)
