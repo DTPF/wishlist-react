2963234fc6b977a0e0b23e296aa1f9cccc476390
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-02 17:32:31 +0200

    change: adapt provider to protected backend

--- src/context/wishlist/WishlistProvider.tsx
+++ src/context/wishlist/WishlistProvider.tsx
@@ -1,28 +1,41 @@
-import { useReducer } from "react";
+import { useContext, useReducer } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import UserContext from "context/user/UserContext";
 import {
 	addWishlistAction,
-	initWishlistAction
+	initWishlistsByUserIdAction
 } from "reducers/wishlist/wishlistActions";
 import WishlistContext from "./WishlistContext";
 import wishlistReducer from "reducers/wishlist/wishlistReducer";
-import initialTracklistState from "./initialWishlistState";
+import initialWishlistState from "./initialWishlistState";
 
-export default function WishlistProvider({ children }: any) {
-	const [tracklistState, dispatch] = useReducer(wishlistReducer, initialTracklistState);
+type Props = {
+  children: React.ReactNode
+}
 
-	const initWishlist = function () {
-		initWishlistAction(dispatch);
+export default function WishlistProvider(props: Props) {
+	const { children } = props;
+	const { dbUser }: any = useContext(UserContext);
+	const [wishlistState, dispatch] = useReducer(wishlistReducer, initialWishlistState);
+	const { getIdTokenClaims, isAuthenticated } = useAuth0();	
+
+	const initWishlistsByUserId = function () {
+		getIdTokenClaims().then((token) => {
+			initWishlistsByUserIdAction(dispatch, isAuthenticated, token, dbUser);
+		})
 	}
 
-	function addWishlist(item: any) {
-		addWishlistAction(dispatch, item, tracklistState);
+	function addNewWishlistItem(item: any) {
+		getIdTokenClaims().then((token) => {
+			addWishlistAction(dispatch, item, wishlistState, isAuthenticated, token);
+		})
 	}
 
 	return (
 		<WishlistContext.Provider value={{
-			...tracklistState,
-			initWishlist,
-			addWishlist
+			...wishlistState,
+			initWishlistsByUserId,
+			addNewWishlistItem
 		}}>
 			{children}
 		</WishlistContext.Provider>
