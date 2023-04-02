c26751e33528a196d7ecdf79482bbe3995ae368d
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-02 18:44:53 +0200

    Update Wishlist page title and fetch data depending on authentication status.

--- src/views/pages/homePage/HomePage.tsx
+++ src/views/pages/homePage/HomePage.tsx
@@ -1,34 +1,39 @@
-import { useContext, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import WishlistContext from 'context/wishlist/WishlistContext';
-import WishlistComponent from 'views/components/wishlistComponent';
-import HelmetSEO from 'utils/helmetSEO';
+import { useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import WishlistContext from 'context/wishlist/WishlistContext'
+import WishlistComponent from 'views/components/wishlistComponent'
+import HelmetSEO from 'utils/helmetSEO'
+import { useAuth0 } from '@auth0/auth0-react'
+import UserContext from 'context/user/UserContext'
 
 export default function HomePage() {
-  const { initWishlist } = useContext(WishlistContext);
-  const params = useParams();
+  const { initWishlistsByUserId } = useContext(WishlistContext)
+  const { dbUser, initGetUser } = useContext(UserContext)
+  const params = useParams()
+  const { isAuthenticated } = useAuth0()
 
   useEffect(() => {
-    initWishlist()
+    initGetUser()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [isAuthenticated])
 
-  const title = () => {
-    if (!params.isCompleted) {
-      return 'Todo'
-    } else if (params.isCompleted === 'active') {
-      return 'Activo'
-    } else {
-      return 'Completado'
-    }
+  useEffect(() => {
+    initWishlistsByUserId()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dbUser._id])
+
+  const setTitle = () => {
+    if (!params.isCompleted) return 'Todo'
+    if (params.isCompleted === 'active') return 'Activo'
+    return 'Completado'
   }
 
   return (
     <HelmetSEO
-      title={`${title()} | Wishlist`}
+      title={`Wishlist | ${setTitle()}`}
       description='Wishlist Page'
     >
       <WishlistComponent params={params} />
     </HelmetSEO>
-  );
+  )
 }
