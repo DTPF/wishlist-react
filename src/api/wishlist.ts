c0bd97e3304f36b395cc07616fd2cd379195d611
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-02 17:23:08 +0200

    change(wishlistApi): adapt to new endpoints
    Update Wishlist API to include userId and authorization token in requests

--- src/api/wishlist.ts
+++ src/api/wishlist.ts
@@ -1,21 +1,28 @@
 import { basePath, apiVersion } from "./utils/config";
 import { WishList } from "interfaces/wishlist";
 
-export const getWishlistApi = async (): Promise<WishList> => {
-	const response = await fetch(`${basePath}/${apiVersion}/get-wishlist`)
+export const getWishlistsByUserIdApi = async (token: string, userId: string): Promise<WishList> => {
+	const params = {
+		method: "GET",
+		headers: {
+			Authorization: `Bearer ${token}`
+		},
+	}
+	const response = await fetch(`${basePath}/${apiVersion}/get-wishlists-by-user-id/${userId}`, params)
 	const data = await response.json()
-	return data as WishList
+	return data
 }
 
-export const postWishlistApi = async (item: any): Promise<WishList> => {
+export const postNewWishlistItemApi = async (wishlistId: string, item: object, token: string): Promise<WishList> => {
 	const params = {
 		method: "POST",
 		headers: {
+			Authorization: `Bearer ${token}`,
 			"Content-Type": "application/json",
 		},
 		body: JSON.stringify(item),
 	}
-	const response = await fetch(`${basePath}/${apiVersion}/post-wishlist-item`, params)
+	const response = await fetch(`${basePath}/${apiVersion}/post-new-wishlist-item/${wishlistId}`, params)
 	const data = await response.json()
 	return data as WishList
 }
