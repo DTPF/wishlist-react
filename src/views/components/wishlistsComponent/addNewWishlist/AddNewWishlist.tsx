bb4c9a1d6f26d079d2c61818135c611be746cd18
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-04 16:49:22 +0200

    feat(addWishlist): create component

---
+++ src/views/components/wishlistsComponent/addNewWishlist/AddNewWishlist.tsx
@@ -0,0 +1,33 @@
+import { useContext, useRef } from 'react'
+import WishlistContext from 'context/wishlist/WishlistContext'
+import UserContext from 'context/user/UserContext'
+import ColorSelect from 'views/UI/colorSelect'
+import { BsPlusCircleDotted } from 'react-icons/bs'
+import './addNewWishlist.scss'
+
+export default function AddNewWishlist() {
+	const { postNewWishlist } = useContext(WishlistContext)
+	const { dbUser } = useContext(UserContext)
+
+	const title: any = useRef(null)
+	return (
+		<div className='add-new-wishlist__add-new-list'>
+			<span
+				ref={title}
+				className="add-new-wishlist__add-new-list__textarea"
+				role="textbox"
+				contentEditable
+				suppressContentEditableWarning={true}
+			></span>
+			<div className="add-new-wishlist__add-new-list__color-select">
+				<ColorSelect />
+			</div>
+			<div className="add-new-wishlist__add-new-list__add-button">
+				<BsPlusCircleDotted onClick={() => {
+					postNewWishlist(dbUser._id, title.current.innerText)
+					title.current.innerText = ''
+				}} />
+			</div>
+		</div>
+	)
+}
