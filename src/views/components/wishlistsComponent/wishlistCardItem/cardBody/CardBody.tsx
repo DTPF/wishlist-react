ef4a5b3e7a5e20df48cb9034193a2f7e3b1ba7f5
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-05 14:57:49 +0200

    Update style and content of CardBody component.

--- src/views/components/wishlistsComponent/wishlistCardItem/cardBody/CardBody.tsx
+++ src/views/components/wishlistsComponent/wishlistCardItem/cardBody/CardBody.tsx
@@ -9,16 +9,26 @@ export default function CardBody({ wishlistItem, setShowPopover }: any) {
 		<div
 			className='wishlist-card-body'
 			onClick={() => setShowPopover(false)}
+			style={{ backgroundColor: `rgba(${wishlistItem.backgroundColor}, 0.08)` }}
 		>
-			<span className='wishlist-card-body__items-completed'>
-				{wishlistItemsCompleted.length}/
-				{wishlistItem.wishlistItems.length} completado
-			</span>
-			{wishlistItem.wishlistItems.map((item: any) => (
-				<p key={item.id} className='wishlist-card-body__item'>
-					{item.isCompleted ? <BsCheckCircleFill /> : <BsCheckCircle />} <span>{item.title}</span>
-				</p>
-			))}
+			{wishlistItem.wishlistItems.length > 0 && (
+				<span
+					className='wishlist-card-body__items-completed'
+					style={{ backgroundColor: `rgba(${wishlistItem.backgroundColor}, 0.3)` }}
+				>
+					{wishlistItemsCompleted.length}/
+					{wishlistItem.wishlistItems.length} completado
+				</span>
+			)}
+			{wishlistItem.wishlistItems.length === 0 ? (
+				<div className='wishlist-card-body__empty-list'>Lista vacía</div>
+			) : (
+				wishlistItem.wishlistItems.map((item: any) => (
+					<p key={item.id} className='wishlist-card-body__item'>
+						{item.isCompleted ? <BsCheckCircleFill /> : <BsCheckCircle />} <span>{item.title}</span>
+					</p>
+				))
+			)}
 		</div>
 	)
 }
