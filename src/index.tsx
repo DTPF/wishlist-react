6ffee54a27bf9645428ab9d6cc2dea39a751f238
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-05 14:46:03 +0200

    Update index to import Spanish locale for moment.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import 'moment/locale/es';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App/>);
