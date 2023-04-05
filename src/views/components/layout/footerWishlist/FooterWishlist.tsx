770c8664a8467b6726a6144c09d08e32d60f7dd7
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-05 14:54:48 +0200

    cahnge(footerWishlist):
    Update FooterWishlist to include total Wishlists and total Wishlists Notes, and display the last modified date

--- src/views/components/layout/footerWishlist/FooterWishlist.tsx
+++ src/views/components/layout/footerWishlist/FooterWishlist.tsx
@@ -1,33 +1,43 @@
-import { useContext } from 'react';
-import WishlistContext from 'context/wishlist/WishlistContext';
-import RemoveWishlist from 'views/components/wishlistComponent/removeWishlist'
-import './footerWishlist.scss'
+import { useContext, useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { IoMdExit } from 'react-icons/io';
+import WishlistContext from 'context/wishlist/WishlistContext';
+import Moment from 'react-moment'
+import './footerWishlist.scss'
 
 export default function FooterWishlist() {
-	const { currentWishlist } = useContext(WishlistContext);
-	const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+	const { wishlistsInfo } = useContext(WishlistContext)
+	const { isAuthenticated } = useAuth0();
+	const [lastModified, setLastModified] = useState("2023-04-05T12:25:50.606Z")
+
+	useEffect(() => {
+		setLastModified(wishlistsInfo.lastModified)
+		const interval = setInterval(function () {
+			setLastModified(wishlistsInfo.lastModified)
+		}, 30000)
+		return () => {
+			clearInterval(interval)
+		}
+	}, [wishlistsInfo.lastModified])
 
 	return (
 		<footer className='footer-wish-list'>
-			<div className='footer-wish-list__authentication'>
-				{!isAuthenticated && (
-					<p>
-						<span onClick={() => loginWithRedirect()}>
-							Identifícate para tener tu propia lista
-						</span>
-					</p>
-				)}
-			</div>
-			<div className='footer-wish-list__remove-list'>
-				{currentWishlist?.wishlistItems?.length > 0 && <RemoveWishlist />}
+			<>
+				<section className='footer-wish-list__info'>
+					<div>
+						<span>{wishlistsInfo.totalWishlists}</span> Listas
+					</div>
+					<div>
+						<span>{wishlistsInfo.totalWishlistsNotes}</span> Notas
+					</div>
+				</section>
 				{isAuthenticated && (
-					<button onClick={() => logout()}>
-						<IoMdExit />
-					</button>
+					<section className='footer-wish-list__info'>
+						<div>
+							Última actualización: <Moment locale="es" fromNow>{new Date(lastModified).valueOf()}</Moment>
+						</div>
+					</section>
 				)}
-			</div>
+			</>
 		</footer>
 	)
 }
