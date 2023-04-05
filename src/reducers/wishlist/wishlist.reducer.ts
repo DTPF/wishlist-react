a377e41813b15b78b0425f2f35b82b28cbf5f9e3
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-05 14:49:20 +0200

    change(wishlistReducer):
    Update to handle wishlistInfo

--- src/reducers/wishlist/wishlist.reducer.ts
+++ src/reducers/wishlist/wishlist.reducer.ts
@@ -9,26 +9,63 @@ export default function wishlistReducer(state: any, action: any) {
 				...state,
 				currentWishlist: payload.currentWishlist,
 				wishlists: payload.wishlists,
+				wishlistsInfo: payload.wishlistsInfo
 			}
 
 		case WishlistTypes.ADD_WISHLIST:
 			return {
 				...state,
-				wishlists: [...state.wishlists, payload]
+				wishlists: [...state.wishlists, payload],
+				wishlistsInfo: {
+					...state.wishlistsInfo,
+					totalWishlists: ++state.wishlistsInfo.totalWishlists,
+					lastModified: Date.now()
+				}
+			}
+
+		case WishlistTypes.REMOVE_WISHLIST:
+			return {
+				...state,
+				wishlists: payload.newWishlist,
+				wishlistsInfo: {
+					...state.wishlistsInfo,
+					totalWishlists: --state.wishlistsInfo.totalWishlists,
+					lastModified: Date.now()
+				}
+			}
+
+		case WishlistTypes.UPDATE_WISHLIST:
+			return {
+				...state,
+				wishlists: payload.newWishlist,
+				wishlistsInfo: {
+					...state.wishlistsInfo,
+					lastModified: Date.now()
+				}
 			}
 
 		case WishlistTypes.ADD_WISHLIST_ITEM:
 			return {
 				...state,
 				currentWishlist: { ...state.currentWishlist, wishlistItems: payload.newWishlistItems },
-				wishlists: payload.newWishlist
+				wishlists: payload.newWishlist,
+				wishlistsInfo: {
+					...state.wishlistsInfo,
+					totalWishlistsNotes: ++state.wishlistsInfo.totalWishlistsNotes,
+					lastModified: Date.now()
+				}
 			}
 
 		case WishlistTypes.REMOVE_WISHLIST_ITEM:
 			return {
 				...state,
 				currentWishlist: payload.newWishlistItems,
-				wishlists: payload.newWishlist
+				wishlists: payload.newWishlist,
+				wishlistsInfo: {
+					...state.wishlistsInfo,
+					totalWishlistsNotes: --state.wishlistsInfo.totalWishlistsNotes,
+					lastModified: Date.now()
+				}
 			}
 
 		case WishlistTypes.SET_CURRENT_WISHLIST:
@@ -41,18 +78,6 @@ export default function wishlistReducer(state: any, action: any) {
 			return {
 				...state,
 				isLoading: payload
-			}
-
-		case WishlistTypes.REMOVE_WISHLIST:
-			return {
-				...state,
-				wishlists: payload.newWishlist
-			}
-
-		case WishlistTypes.UPDATE_WISHLIST:
-			return {
-				...state,
-				wishlists: payload.newWishlist
 			}
 
 		default:
