1e587b5c834ec004f29f75e3935987d788bea3dc
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-05 14:57:04 +0200

    Add OutsideClickHandler to WishlistCardItem component.

--- src/views/components/wishlistsComponent/wishlistCardItem/WishlistCardItem.tsx
+++ src/views/components/wishlistsComponent/wishlistCardItem/WishlistCardItem.tsx
@@ -3,15 +3,18 @@ import CardHeader from './cardHeader'
 import CardBody from './cardBody'
 import { CardFooter } from './cardFooter'
 import './wishlistCardItem.scss'
+import OutsideClickHandler from 'react-outside-click-handler';
 
 export default function WishlistCardItem({ wishlistItem }: any) {
 	const [showPopover, setShowPopover] = useState(false)
 
 	return (
-		<div className='wishlist-card-item'>
-			<CardHeader wishlistItem={wishlistItem} />
-			<CardBody wishlistItem={wishlistItem} setShowPopover={setShowPopover} />
-			<CardFooter wishlistItem={wishlistItem} showPopover={showPopover} setShowPopover={setShowPopover} />
-		</div>
+		<OutsideClickHandler onOutsideClick={() => setShowPopover(false)}>
+			<div className='wishlist-card-item'>
+				<CardHeader wishlistItem={wishlistItem} />
+				<CardBody wishlistItem={wishlistItem} setShowPopover={setShowPopover} />
+				<CardFooter wishlistItem={wishlistItem} showPopover={showPopover} setShowPopover={setShowPopover} />
+			</div>
+		</OutsideClickHandler>
 	)
 }
