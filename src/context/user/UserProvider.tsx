200988a6c480327d1a842fa95bf925bd991c75ed
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-06 13:35:18 +0200

    change(userProvider): add useMemo and useCallback

--- src/context/user/UserProvider.tsx
+++ src/context/user/UserProvider.tsx
@@ -1,14 +1,14 @@
-import { useReducer } from "react";
+import { useCallback, useEffect, useMemo, useReducer } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import {
 	initGetUserAction
 } from "reducers/user/user.actions";
 import UserContext from "./UserContext";
 import userReducer from "reducers/user/user.reducer";
 import initialUserState from "./initialUserState";
-import { useAuth0 } from "@auth0/auth0-react";
 
 type Props = {
-  children: React.ReactNode
+	children: React.ReactNode
 }
 
 export default function UserProvider(props: Props) {
@@ -16,17 +16,27 @@ export default function UserProvider(props: Props) {
 	const [userState, dispatch] = useReducer(userReducer, initialUserState);
 	const { getIdTokenClaims, isAuthenticated } = useAuth0();
 
-	const initGetUser = function () {
+	useEffect(() => {
+		initGetUser()
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [isAuthenticated])
+
+	const initGetUser = useCallback(() => {
 		getIdTokenClaims().then(auth0User => {
 			initGetUserAction(dispatch, isAuthenticated, auth0User);
-		})
-	}
+		});
+	}, [getIdTokenClaims, isAuthenticated]);
+
+	const memoProvider = useMemo(
+		() => ({
+			...userState,
+			initGetUser
+		}),
+		[userState, initGetUser]
+	);
 
 	return (
-		<UserContext.Provider value={{
-			...userState,
-			initGetUser,
-		}}>
+		<UserContext.Provider value={memoProvider}>
 			{children}
 		</UserContext.Provider>
 	)
