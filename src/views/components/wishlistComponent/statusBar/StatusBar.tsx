8089e23d72d991c0183aedbafc25e08c71c1b431
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-07 16:11:11 +0200

    Update GetWishlistLength to use length prop instead of currentWishlist.

--- src/views/components/wishlistComponent/statusBar/StatusBar.tsx
+++ src/views/components/wishlistComponent/statusBar/StatusBar.tsx
@@ -5,21 +5,23 @@ import './statusBar.scss'
 
 export default function StatusBar() {
 	const { currentWishlist } = useContext(WishlistContext);
+	const filteredActive = currentWishlist.wishlistItems.filter((item: any) => item.isCompleted === false)
+	const filteredCompleted = currentWishlist.wishlistItems.filter((item: any) => item.isCompleted === true)	
 
 	return (
 		<div className='status-bar'>
 			<div className='status-bar__nav'>
 				<NavLink to={'/wishlist/'}>
 					<RenderButton title='Todo' />
-					<GetWishlistLength currentWishlist={currentWishlist} />
+					<GetWishlistLength length={currentWishlist.wishlistItems.length} />
 				</NavLink>
 				<NavLink to={'/wishlist/active'}>
 					<RenderButton title='Activo' />
-					<GetWishlistLength currentWishlist={currentWishlist} />
+					<GetWishlistLength length={filteredActive.length} />
 				</NavLink>
 				<NavLink to={'/wishlist/completed'}>
 					<RenderButton title='Completado' />
-					<GetWishlistLength currentWishlist={currentWishlist} />
+					<GetWishlistLength length={filteredCompleted.length} />
 				</NavLink>
 			</div>
 		</div>
@@ -34,6 +36,6 @@ function RenderButton({ title }: any) {
 	)
 }
 
-function GetWishlistLength({ currentWishlist }: any) {
-	return <div className='status-bar__nav--items-count'>{currentWishlist.wishlistItems.length}</div>
+function GetWishlistLength({ length }: any) {
+	return <div className='status-bar__nav--items-count'>{length}</div>
 }
