4f54062cffb87bc5e17632c35138f97e33385bf2
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-09 13:39:30 +0200

    Add authentication button and popover for user profile information

--- src/views/components/layout/footerWishlist/FooterWishlist.tsx
+++ src/views/components/layout/footerWishlist/FooterWishlist.tsx
@@ -1,13 +1,15 @@
 import { useContext, useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import WishlistContext from 'context/wishlist/WishlistContext';
+import OutsideClickHandler from 'react-outside-click-handler';
 import Moment from 'react-moment'
 import './footerWishlist.scss'
 
 export default function FooterWishlist() {
 	const { wishlistsInfo } = useContext(WishlistContext)
-	const { isAuthenticated } = useAuth0();
+	const { user, loginWithRedirect, isAuthenticated, logout }: any = useAuth0()
 	const [lastModified, setLastModified] = useState("2023-04-05T12:25:50.606Z")
+	const [showPopover, setShowPopover] = useState(false)
 
 	useEffect(() => {
 		setLastModified(wishlistsInfo.lastModified)
@@ -21,23 +23,46 @@ export default function FooterWishlist() {
 
 	return (
 		<footer className='footer-wish-list'>
-			<>
-				<section className='footer-wish-list__info'>
+			<section className='footer-wish-list__wishlists-info'>
+				<div>
+					<span>{wishlistsInfo.totalWishlists}</span> Listas
+				</div>
+				<div>
+					<span>{wishlistsInfo.totalWishlistsNotes}</span> Notas
+				</div>
+			</section>
+			{isAuthenticated && (
+				<section className='footer-wish-list__last-updated'>
 					<div>
-						<span>{wishlistsInfo.totalWishlists}</span> Listas
-					</div>
-					<div>
-						<span>{wishlistsInfo.totalWishlistsNotes}</span> Notas
+						Actualizado: <Moment locale="es" fromNow>{new Date(lastModified).valueOf()}</Moment>
 					</div>
 				</section>
-				{isAuthenticated && (
-					<section className='footer-wish-list__info'>
-						<div>
-							Última actualización: <Moment locale="es" fromNow>{new Date(lastModified).valueOf()}</Moment>
-						</div>
-					</section>
+			)}
+
+			<button onClick={user ? undefined : () => loginWithRedirect()} className='footer-wish-list__authentication'>
+				{user ? (
+					<div onClick={() => setShowPopover(!showPopover)} className='footer-wish-list__authentication--profile-image'>
+						<img onClick={() => setShowPopover(!showPopover)} src={user?.picture} alt={`Imagen de usuario de ${user?.nickname}`} />
+					</div>
+				) : (
+					<p>Identifícate</p>
 				)}
-			</>
+				{showPopover && (
+					<OutsideClickHandler onOutsideClick={() => setShowPopover(false)}>
+						<span className='footer-wish-list__authentication--popover'>
+							<div className='footer-wish-list__authentication--popover__content'>
+								<div className='footer-wish-list__authentication--popover__content--language'>
+									🇪🇸 | 🇬🇧
+								</div>
+								<div onClick={() => logout()} className='footer-wish-list__authentication--popover__content--logout'>
+									<p>Cerrar sesión</p>
+								</div>
+							</div>
+						</span>
+					</OutsideClickHandler>
+				)}
+			</button>
+
 		</footer>
 	)
 }
