eb0835ca03d247a7b760276ff5408435623e97e6
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-12 10:46:06 +0200

    feat(serviceWorker): add workbox serviceWorkers

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { toast } from 'react-hot-toast';
 import 'moment/locale/es';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App/>);
 
+serviceWorkerRegistration.register({
+  onUpdate: (registration: any) => {
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      registration.update();
+			toast("Nueva versión. Actualizando la web")
+			window.location.reload();
+    }
+  }
+});
