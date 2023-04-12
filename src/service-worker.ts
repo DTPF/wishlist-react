a7efe8ac725dcc2e835c6185756f6be843e920b0
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-12 12:24:26 +0200

    Update service worker to include file extension regexp for resource requests.

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -21,6 +21,8 @@ precacheAndRoute(self.__WB_MANIFEST);
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
+const fileExtensionRegexp = /\.(html|js|css|png|jpe?g|gif|svg|woff2?|ttf|eot)$/i;
+
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }: { request: Request; url: URL }) => {
@@ -36,7 +38,7 @@ registerRoute(
 
     // If this looks like a URL for a resource, because it contains
     // a file extension, skip.
-    if (/[^/?]+\\.[^/]+$/.test(url.pathname)) {
+    if (url.pathname.match(fileExtensionRegexp)) {
       return false;
     }
 
