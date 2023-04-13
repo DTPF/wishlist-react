9c6503d15210a0b3885087a42087c22118ef2088
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-13 19:01:07 +0200

    Update HeaderWishlist component to display DaList and add UserProfile component

--- src/views/components/layout/headerWishlist/HeaderWishlist.tsx
+++ src/views/components/layout/headerWishlist/HeaderWishlist.tsx
@@ -1,16 +1,12 @@
-import { useContext } from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
-import UserContext from 'context/user/UserContext'
+import UserProfile from 'views/components/userProfile'
 import './headerWishlist.scss'
 
 export default function HeaderWishlist() {
-  const { user }: any = useAuth0()
-  const { dbUser }: any = useContext(UserContext)
-
   return (
     <div className='header-wishlist'>
+      <UserProfile />
       <div className='header-wishlist__title'>
-        <h1>Lista de {dbUser.name ? dbUser.name : user?.given_name}</h1>
+        <h1>DaList</h1>
       </div>
     </div>
   )
