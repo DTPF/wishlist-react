73d501d13dc6a2a0594b379eb78bd910362d6192
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-13 19:02:04 +0200

    Update sorting of wishlists based on user preferences

--- src/views/components/wishlistsComponent/WishlistsComponent.tsx
+++ src/views/components/wishlistsComponent/WishlistsComponent.tsx
@@ -1,50 +1,54 @@
-import { useContext, useState } from 'react'
-import { useAuth0 } from '@auth0/auth0-react';
-import UserContext from 'context/user/UserContext';
+import { useContext } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+import UserContext from 'context/user/UserContext'
 import WishlistContext from 'context/wishlist/WishlistContext'
-import AddNewWishlist from './addNewWishlist';
-import WishlistCardItem from './wishlistCardItem';
-import Spinner from 'views/UI/spinner';
-import { FiSettings } from 'react-icons/fi';
-import { FaPlusCircle } from 'react-icons/fa';
-import { HiOutlineArrowDown, HiOutlineArrowUp } from 'react-icons/hi';
+import AddNewWishlist from './addNewWishlist'
+import WishlistCardItem from './wishlistCardItem'
+import FilterDropdown from './filterDropdown/FilterDropdown'
+import Spinner from 'views/UI/spinner'
+import { FaPlusCircle } from 'react-icons/fa'
+import { FolderViewOutlined } from '@ant-design/icons'
 import './wishlistsComponent.scss'
+import { Button, Empty } from 'antd'
 
 export default function WishlistsComponent() {
-	const { wishlists, isLoading, postNewWishlist }: any = useContext(WishlistContext)
+	const { wishlists, isLoading, postNewWishlist } = useContext(WishlistContext)
 	const { dbUser } = useContext(UserContext)
-	const { isLoading: isLoadingAuth0, isAuthenticated } = useAuth0();
-	const [viewDirection, setViewDirection] = useState('updatedAt')
-	const [viewDescentetDirection, setViewDescentetDirection] = useState(true)
+	const { isLoading: isLoadingAuth0, isAuthenticated } = useAuth0()
 
-	switch (viewDirection) {
+	const { wishlistsOrder, wishlistsDirection } = dbUser.wishlistsInfo
+
+	switch (wishlistsOrder) {
 		case 'updatedAt':
-			wishlists.sort(function (a: any, b: any) {
-				const c: any = new Date(a.updatedAt);
-				const d: any = new Date(b.updatedAt);
-				if (viewDescentetDirection) return d - c;
+			wishlists.sort(function (a: { updatedAt: string | number | Date }, b: { updatedAt: string | number | Date }) {
+				const c: any = new Date(a.updatedAt)
+				const d: any = new Date(b.updatedAt)
+				if (wishlistsDirection === 'desc') return d - c
 				return c - d
 			})
-			break;
+			break
 
 		case 'createdAt':
-			wishlists.sort(function (a: any, b: any) {
-				const c: any = new Date(a.createdAt);
-				const d: any = new Date(b.createdAt);
-				if (viewDescentetDirection) return d - c;
+			wishlists.sort(function (a: { createdAt: string | number | Date }, b: { createdAt: string | number | Date }) {
+				const c: any = new Date(a.createdAt)
+				const d: any = new Date(b.createdAt)
+				if (wishlistsDirection === 'desc') return d - c
 				return c - d
 			})
-			break;
+			break
 
 		case 'name':
-			wishlists.sort((a: any, b: any) => {
-				if (viewDescentetDirection) return a.wishlistName.localeCompare(b.wishlistName)
-				return b.wishlistName.localeCompare(a.wishlistName)
+			wishlists.sort((a: { wishlistName: string }, b: { wishlistName: string }) => {
+				if (wishlistsDirection === 'desc') return b.wishlistName.localeCompare(a.wishlistName)
+				return a.wishlistName.localeCompare(b.wishlistName)
 			})
-			break;
+			break
 
 		default:
-			throw new Error()
+			wishlists.sort((a: { wishlistName: string }, b: { wishlistName: string }) => {
+				if (wishlistsDirection === 'desc') return b.wishlistName.localeCompare(a.wishlistName)
+				return a.wishlistName.localeCompare(b.wishlistName)
+			})
 	}
 
 	return (
@@ -56,40 +60,51 @@ export default function WishlistsComponent() {
 			) : (
 				<section className='wishlists-component'>
 					<div className='wishlists-component__filters'>
-						<div className='wishlists-component__filters--left-side'>
-							<div className='wishlists-component__filters--left-side__select'>
-								<select name="select" onChange={(e) => setViewDirection(e.target.value)}>
-									<option value="updatedAt">Última actualización</option>
-									<option value="name">Nombre</option>
-									<option value="createdAt">Fecha de creación</option>
-								</select>
-							</div>
-							<div className='wishlists-component__filters--left-side__view-direction'>
-								<span onClick={() => setViewDescentetDirection(!viewDescentetDirection)}>
-									{viewDescentetDirection ? <HiOutlineArrowDown /> : <HiOutlineArrowUp />}
-								</span>
-							</div>
-						</div>
-						<div className='wishlists-component__filters--right-side'>
-							<FiSettings />
-						</div>
+						{wishlists.length > 0 && (
+							<>
+								<div className='wishlists-component__filters--left-side'>
+									<div className='wishlists-component__filters--left-side__select'>
+										<FilterDropdown />
+									</div>
+								</div>
+								<div className='wishlists-component__filters--right-side'>
+									<FolderViewOutlined />
+								</div>
+							</>
+						)}
 					</div>
 
 					<div className='wishlists-component__items'>
-						{wishlists.map((item: any) => (
-							<WishlistCardItem
-								key={item._id}
-								wishlistItem={item}
-							/>
-						))}
-						<div className='wishlists-component__add-item-desktop'>
-							<AddNewWishlist />
-						</div>
+						{wishlists.length === 0 ? (
+							<Empty
+								image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+								imageStyle={{ height: 200 }}
+								description={
+									<span>
+										No hay nada que mostrar
+									</span>
+								}
+							>
+								<Button onClick={() => postNewWishlist(dbUser._id, null)} type="primary">Crear lista</Button>
+							</Empty>
+						) : (
+							<>
+								{wishlists.map((item: { _id: string }) => (
+									<WishlistCardItem
+										key={item._id}
+										wishlistItem={item}
+									/>
+								))}
+								<div className='wishlists-component__add-item-desktop'>
+									<AddNewWishlist />
+								</div>
+							</>
+						)}
 					</div>
 					<div className='wishlists-component__add-item-mobile'>
 						<FaPlusCircle onClick={() => {
-							postNewWishlist(dbUser._id)
-							window.scrollTo({ top: 0, behavior: 'smooth' });
+							postNewWishlist(dbUser._id, null)
+							window.scrollTo({ top: 0, behavior: 'smooth' })
 						}}
 						/>
 					</div>
