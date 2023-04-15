f0566bc2a847af189cd3b8b1e233793beb420baa
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-15 12:36:28 +0200

    change(index.js): remove serviceworker messages

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import { toast } from 'react-hot-toast';
+import './utils/i18n';
 import 'moment/locale/es';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App/>);
+root.render(<App />);
 
 serviceWorkerRegistration.register({
   onUpdate: (registration: any) => {
     if (registration && registration.waiting) {
       registration.waiting.postMessage({ type: "SKIP_WAITING" });
       registration.update();
-			toast("Nueva versión. Actualizando la web")
-			window.location.reload();
     }
   }
 });
