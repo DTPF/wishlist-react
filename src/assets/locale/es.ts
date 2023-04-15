e6792c9303c7c5f92ee57d428165e7c3672c33ba
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-15 12:39:37 +0200

    feat(languages): add Spanish translation

---
+++ src/assets/locale/es.ts
@@ -0,0 +1,57 @@
+const spanishTranslation = {
+  // Profile popover
+  logout: 'Cerrar sesión',
+  login: 'Identifícate',
+  titleProfilePopover: 'Opciones de usuario',
+
+  // Profile form
+  titleProfileModal: 'Configuración de',
+  titlePopConfirm: 'Eliminar cuenta de usuario',
+  contentPopConfirm: 'Se eliminarán todos los datos, incluídas las listas.',
+  nameInput: 'Nombre',
+  lastnameInput: 'Apellidos',
+  languageInput: 'Idioma',
+  languageEnglish: 'Inglés',
+  languageSpanish: 'Español',
+
+  // Wishlists - Home
+  htmlTitleHome: 'Dalist - Listas',
+  htmlDescriptionHome: 'Dalist página principal',
+  emptyWishlists: 'No hay nada que mostrar',
+  createWishlist: 'Crear lista',
+  createWishlistPlaceholder: 'Nueva lista',
+  // Cards
+  completedItems: 'completado',
+  deleteList: 'Eliminar lista',
+  lastModified: 'Editado:',
+  // Filters
+  filterUpdatedAt: 'Última actualización',
+  filterName: 'Nombre',
+  filterCreatedAt: 'Fecha de creación',
+  filterOrder: 'Orden:',
+  filterOrderAsc: 'Ascendente',
+  filterOrderDesc: 'Descendente',
+
+  // Wishlist - WishlistPage
+  htmlDescriptionWishlist: 'Dalist página de notas',
+  selectStatusAll: 'Todo',
+  selectStatusActive: 'Activo',
+  selectStatusComplete: 'Completado',
+  postNotePlaceholder: 'Introduce el título de la lista',
+  postNoteButton: 'Guadar',
+
+  // Footer
+  lists: 'Listas',
+  notes: 'Notas',
+  dateLabel: 'Actualizado:',
+
+  // Shared
+  settings: 'Ajustes',
+  delete: 'Borrar',
+  deleteAccount: 'Eliminar cuenta',
+  cancel: 'Cancelar',
+  save: 'Guardar',
+  emptyMessage: 'Lista vacía',
+}
+
+export default spanishTranslation;
