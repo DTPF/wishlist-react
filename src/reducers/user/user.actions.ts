82c9ad887eadc1d3d075069a72548a09c71ddf3d
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-17 18:59:38 +0200

    change(userAction):
    Update user action and init get user action to include support for theme color and language

--- src/reducers/user/user.actions.ts
+++ src/reducers/user/user.actions.ts
@@ -1,57 +1,34 @@
 import * as UserTypes from "reducers/user/user.types";
 import { initGetUserAPI, updateUserApi } from "api/user.api";
 
-export const initGetUserAction =
-	async function (
-		dispatch: any,
-		isAuthenticated: boolean,
-		auth0User: any,
-		i18n: any,
-		isLoading: boolean
-	) {
-		if (!isLoading && isAuthenticated && auth0User.sub) {
-			try {
-				const response = await initGetUserAPI(auth0User.__raw, { userId: auth0User.sub });
-
-				if (response.status === 200 || response.status === 201) {
-					i18n.changeLanguage(response.user.language)
-					return dispatch({
-						type: UserTypes.INIT_GET_USER,
-						payload: {
-							auth0User: auth0User,
-							dbUser: response.user
-						}
-					});
-				} else {
-					throw new Error()
-				}
-			} catch (err) {
-				throw new Error()
-			}
-		} else {
-			if (!isLoading) {
-				return dispatch({
-					type: UserTypes.SET_GUESS,
-					payload: true
-				})
-			}
-		}
+export const initGetUserAction = async function (
+	dispatch: any,
+	isAuthenticated: boolean,
+	auth0User: any,
+	i18n: any,
+	isLoading: boolean
+) {
+	const themeLS = localStorage.getItem('theme-color')
+	if (themeLS) {
+		document.getElementsByTagName<any>("META")[2].content = (themeLS)
 	}
-
-export const updateUserAction =
-	async function (
-		dispatch: any,
-		userId: string,
-		data: any,
-		token: any
-	) {
+	const initServices = (response: any) => {
+		const { language, colorPrimaryBg } = response.user.appInfo
+		i18n.changeLanguage(language)
+		localStorage.setItem('theme-color', colorPrimaryBg)
+		document.getElementsByTagName<any>("META")[2].content = colorPrimaryBg
+	}
+	if (!isLoading && isAuthenticated && auth0User.sub) {
 		try {
-			const response = await updateUserApi(userId, data, token.__raw);
-
-			if (response.status === 200) {
+			const response = await initGetUserAPI(auth0User.__raw, { userId: auth0User.sub });
+			if (response.status === 200 || response.status === 201) {
+				initServices(response)
 				return dispatch({
-					type: UserTypes.UPDATE_USER,
-					payload: response.user
+					type: UserTypes.INIT_GET_USER,
+					payload: {
+						auth0User: auth0User,
+						dbUser: response.user
+					}
 				});
 			} else {
 				throw new Error()
@@ -59,4 +36,34 @@ export const updateUserAction =
 		} catch (err) {
 			throw new Error()
 		}
+	} else {
+		if (!isLoading) {
+			return dispatch({
+				type: UserTypes.SET_GUESS,
+				payload: true
+			})
+		}
 	}
+}
+
+export const updateUserAction = async function (
+	dispatch: any,
+	userId: string,
+	data: any,
+	token: any
+) {
+	try {
+		const response = await updateUserApi(userId, data, token.__raw);
+
+		if (response.status === 200) {
+			return dispatch({
+				type: UserTypes.UPDATE_USER,
+				payload: response.user
+			});
+		} else {
+			throw new Error()
+		}
+	} catch (err) {
+		throw new Error()
+	}
+}
