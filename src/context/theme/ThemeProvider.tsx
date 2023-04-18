24a02637348db7170e4fb14d1b5873e247f5383d
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-18 09:19:47 +0200

    Update ThemeProvider to utilize useCallback and useMemo hooks.

--- src/context/theme/ThemeProvider.tsx
+++ src/context/theme/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import ThemeContext from './ThemeContext'
 import invertHexColor from 'utils/invertHexColor'
 import { InitialThemeContextType } from './initialThemeState'
@@ -6,20 +6,30 @@ import { ChildrenProps } from 'interfaces/globals'
 
 export default function ThemeProvider({ children }: ChildrenProps) {
 	const themeLS = localStorage.getItem('theme-color')
+	const theme = themeLS ? themeLS : '#232F3E'
 	const [currentThemeColor, setThemeColor] = useState<InitialThemeContextType>({
-		colorPrimary: themeLS ? invertHexColor(themeLS ? themeLS : '#232F3E', true) : '#fff',
+		colorPrimary: themeLS ? invertHexColor(theme, true) : '#fff',
 		colorPrimaryBg: themeLS ? themeLS : '#232F3E'
 	})
-	const setThemeColorAction = (colorPrimary: string, colorPrimaryBg: string) => {
+
+	const setThemeColorAction = useCallback(async (colorPrimary: string, colorPrimaryBg: string) => {
 		setThemeColor({
-			colorPrimary,
-			colorPrimaryBg
+			colorPrimary: colorPrimary ? colorPrimary : '#fff',
+			colorPrimaryBg: colorPrimaryBg ? colorPrimaryBg : '#232F3E'
 		})
 		document.getElementsByTagName<any>("META")[2].content = colorPrimaryBg
-	}
+	}, []);
+
+	const memoProvider = useMemo(
+		() => ({
+			currentThemeColor,
+			setThemeColorAction,
+		}),
+		[currentThemeColor, setThemeColorAction]
+	);
 
 	return (
-		<ThemeContext.Provider value={{ currentThemeColor, setThemeColorAction }}>
+		<ThemeContext.Provider value={memoProvider}>
 			{children}
 		</ThemeContext.Provider>
 	)
