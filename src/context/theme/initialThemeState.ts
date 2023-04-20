d9424facba30da3136bde0e3c36ca6ca9ae66534
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-20 18:31:45 +0200

    change(initialThemeState): add wishlist colors

--- src/context/theme/initialThemeState.ts
+++ src/context/theme/initialThemeState.ts
@@ -1,14 +1,33 @@
+import invertHexColor from "utils/invertHexColor";
+const themeLS = localStorage.getItem('theme-color')
+
 export interface InitialThemeContextType {
-	colorPrimary: string;
-	colorPrimaryBg: string
+	colorPrimary: string
+	colorPrimaryBg: string,
+	wishlistColor: string,
+	wishlistColorBg: string
 }
 
 export interface UpdateCurrentThemeContextType {
 	currentThemeColor: InitialThemeContextType;
-	setThemeColorAction: (colorPrimary: string, colorPrimaryBg: string) => void
+	setAppThemeColorAction: (color: string) => void
+	setWishlistThemeColorAction: (color: string) => void
+}
+
+export const initialThemeContext = {
+	currentThemeColor: {
+		colorPrimary: '',
+		colorPrimaryBg: '',
+		wishlistColor: '',
+		wishlistColorBg: ''
+	},
+	setAppThemeColorAction: (color: string) => { },
+	setWishlistThemeColorAction: (color: string) => { }
 }
 
 export const initialThemeState = {
-	currentThemeColor: { colorPrimary: '', colorPrimaryBg: '' },
-	setThemeColorAction: (colorPrimary: string, colorPrimaryBg: string) => { }
+	colorPrimary: themeLS ? invertHexColor(themeLS) : '#fff',
+	colorPrimaryBg: themeLS ? themeLS : '#232F3E',
+	wishlistColor: '',
+	wishlistColorBg: ''
 }
