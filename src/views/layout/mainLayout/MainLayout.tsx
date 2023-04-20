ccd5774fbc6377cb7d779fb1375eab918b31321e
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-20 19:04:11 +0200

    refactor(mainLayout): to reduce cognitive complexity

--- src/views/layout/mainLayout/MainLayout.tsx
+++ src/views/layout/mainLayout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from 'react';
 import { Outlet } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react';
-import { Layout } from 'antd';
+import { Layout, Switch } from 'antd';
 import ThemeContext from 'context/theme/ThemeContext';
 import AppSettingsModalContext from 'context/appSettingsModal/AppSettingsModalContext';
 import FooterWishlist from 'views/components/layout/footerWishlist'
@@ -11,63 +11,26 @@ import ApplicationSettings from 'views/components/applicationSettings/Applicatio
 import { useTranslation } from 'react-i18next';
 import { LoginOutlined, LogoutOutlined, MenuFoldOutlined, MenuUnfoldOutlined, SettingOutlined } from '@ant-design/icons';
 import './mainLayout.scss'
+const moonImage = require('assets/images/moon.png')
+const sunImage = require('assets/images/sun.png')
 
 export default function MainLayout() {
-	const { user, loginWithRedirect, logout }: any = useAuth0()
 	const [navbarCollapsed, setNavbarCollapsed] = useState(true)
-	const { t: translate } = useTranslation();
-	const { Header, Content, Footer, Sider } = Layout;
-	const { currentThemeColor } = useContext(ThemeContext)
-	const { colorPrimary, colorPrimaryBg } = currentThemeColor
-	const { setOpenSettingsModal }: any = useContext(AppSettingsModalContext)
+	const { Header, Content, Footer } = Layout;
 
 	return (
 		<Layout className='main-layout'>
 			<ApplicationSettings />
-			<Sider
-				trigger={null}
-				breakpoint="lg"
-				collapsedWidth="0"
-				collapsed={navbarCollapsed}
-				className='main-layout__sider'
-				style={{ backgroundColor: colorPrimaryBg }}
-			>
-				<ul className='main-layout__sider--menu'>
-					<div className='main-layout__sider--menu__language'><Language /></div>
-					<li onClick={() => {
-						setNavbarCollapsed(true)
-						setOpenSettingsModal(true)
-					}} style={{ color: colorPrimary }}>
-						<SettingOutlined /> {translate('settings')}
-					</li>
-					{user ? (
-						<li onClick={logout} style={{ color: colorPrimary }}>
-							<LogoutOutlined />
-							{translate('logout')}
-						</li>
-					) : (
-						<li onClick={loginWithRedirect} style={{ color: colorPrimary }}>
-							<LoginOutlined />
-							{translate('login')}
-						</li>
-					)}
-				</ul>
-			</Sider>
+			<SiderNavBar
+				navbarCollapsed={navbarCollapsed}
+				setNavbarCollapsed={setNavbarCollapsed}
+			/>
 			<Layout className='main-layout__container'>
 				<Header className='main-layout__container--header'>
-					{navbarCollapsed ? (
-						<MenuUnfoldOutlined
-							className='main-layout__container--header__icon'
-							onClick={() => setNavbarCollapsed(!navbarCollapsed)}
-							style={{ color: colorPrimary }}
-						/>
-					) : (
-						<MenuFoldOutlined
-							className='main-layout__container--header__icon'
-							onClick={() => setNavbarCollapsed(!navbarCollapsed)}
-							style={{ color: colorPrimary }}
-						/>
-					)}
+					<MenuIcon
+						navbarCollapsed={navbarCollapsed}
+						setNavbarCollapsed={setNavbarCollapsed}
+					/>
 					<HeaderWishlist />
 				</Header>
 				<Content
@@ -80,5 +43,93 @@ export default function MainLayout() {
 				</Footer>
 			</Layout>
 		</Layout>
+	)
+}
+
+function SiderNavBar({ navbarCollapsed, setNavbarCollapsed }: any) {
+	const { user, loginWithRedirect, logout }: any = useAuth0()
+	const { currentThemeColor } = useContext(ThemeContext)
+	const { colorPrimary, colorPrimaryBg } = currentThemeColor
+	const { t: translate } = useTranslation();
+	const { Sider } = Layout;
+	const { setOpenSettingsModal }: any = useContext(AppSettingsModalContext)
+
+	return (
+		<Sider
+			trigger={null}
+			breakpoint="lg"
+			collapsedWidth="0"
+			collapsed={navbarCollapsed}
+			className='main-layout__sider'
+			style={{ backgroundColor: colorPrimaryBg }}
+		>
+			<ul className='main-layout__sider--menu'>
+				<div className='main-layout__sider--menu__top'>
+					<div className='main-layout__sider--menu__top--language'>
+						<Language />
+					</div>
+					<div className='main-layout__sider--menu__top--theme'>
+						<Switch
+							checkedChildren={
+								<img
+									src={moonImage}
+									className='main-layout__sider--menu__top--theme__moon'
+									alt='Light Theme'
+									width='23'
+									height='23'
+								/>
+							}
+							unCheckedChildren={
+								<img
+									src={sunImage}
+									className='main-layout__sider--menu__top--theme__sun'
+									alt='Light Theme'
+									width='22'
+									height='22'
+								/>
+							}
+							defaultChecked={false}
+						/>
+					</div>
+				</div>
+				<li onClick={() => {
+					setNavbarCollapsed(true)
+					setOpenSettingsModal(true)
+				}} style={{ color: colorPrimary }}>
+					<SettingOutlined /> {translate('settings')}
+				</li>
+				{user ? (
+					<li onClick={logout} style={{ color: colorPrimary }}>
+						<LogoutOutlined />
+						{translate('logout')}
+					</li>
+				) : (
+					<li onClick={loginWithRedirect} style={{ color: colorPrimary }}>
+						<LoginOutlined />
+						{translate('login')}
+					</li>
+				)}
+			</ul>
+		</Sider>
+	)
+}
+
+function MenuIcon({ navbarCollapsed, setNavbarCollapsed }: any) {
+	const { currentThemeColor } = useContext(ThemeContext)
+	const { colorPrimary } = currentThemeColor
+	return (
+		navbarCollapsed ? (
+			<MenuUnfoldOutlined
+				className='main-layout__container--header__icon'
+				onClick={() => setNavbarCollapsed(!navbarCollapsed)}
+				style={{ color: colorPrimary }}
+			/>
+		) : (
+			<MenuFoldOutlined
+				className='main-layout__container--header__icon'
+				onClick={() => setNavbarCollapsed(!navbarCollapsed)}
+				style={{ color: colorPrimary }}
+			/>
+		)
 	)
 }
