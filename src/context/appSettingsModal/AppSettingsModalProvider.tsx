ff8f08c3295c9ce42965e6ebf5ac2ddace04c7ec
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-20 19:54:44 +0200

    refactor(appModal): add useMemo

--- src/context/appSettingsModal/AppSettingsModalProvider.tsx
+++ src/context/appSettingsModal/AppSettingsModalProvider.tsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import AppSettingsModalContext from './AppSettingsModalContext'
 
 export default function AppSettingsModalProvider({ children }: any) {
 	const [openSettingsModal, setOpenSettingsModal] = useState(false)
 
+	const memoProvider = useMemo(
+		() => ({
+			openSettingsModal,
+			setOpenSettingsModal,
+		}),
+		[
+			openSettingsModal,
+			setOpenSettingsModal,
+		]
+	);
+
 	return (
-		<AppSettingsModalContext.Provider value={{ openSettingsModal, setOpenSettingsModal }}>
+		<AppSettingsModalContext.Provider value={memoProvider}>
 			{children}
 		</AppSettingsModalContext.Provider>
 	)
