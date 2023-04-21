8d0af89d147496d8aaf900d9fcec6b8ac9cd5a64
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-21 12:24:59 +0200

    change(colorPickerApp):
    Update ColorPickerApp to use ChromePicker for selecting application color

--- src/views/UI/colorPickerApp/ColorPickerApp.tsx
+++ src/views/UI/colorPickerApp/ColorPickerApp.tsx
@@ -1,26 +1,13 @@
 import { useContext } from 'react'
 import ThemeContext from 'context/theme/ThemeContext';
-import { Form, Input } from 'antd';
+import { ChromePicker as ColorPicker } from 'react-color';
 
 export default function ColorPickerApp() {
 	const { currentThemeColor, setAppThemeColorAction } = useContext(ThemeContext)
-
 	return (
-		<Form
-			labelCol={{ span: 12 }}
-			wrapperCol={{ span: 24 }}
-			style={{ maxWidth: 600, marginBottom: 0, display: 'flex', alignItems: 'center' }}
-		>
-			<Form.Item
-				label='Color de la aplicación'
-			>
-				<Input
-					type='color'
-					value={currentThemeColor.colorPrimaryBg}
-					onChange={(e: any) => setAppThemeColorAction(e.target.value)}
-					style={{ padding: 0, border: 0, width: 150, height: 35, cursor: 'pointer' }}
-				/>
-			</Form.Item>
-		</Form>
+		<ColorPicker
+			color={currentThemeColor.colorPrimaryBg}
+			onChange={(e) => setAppThemeColorAction(e.hex)}
+		/>
 	)
 }
