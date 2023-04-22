899885523fd50fd2ce2d3425777912676ef40d68
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-22 14:06:32 +0200

    refactor(api's): to use getParams function

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,84 +1,43 @@
 import { basePath, apiVersion } from "./utils/config";
+import getParams from "./utils/getParams";
 
 export const initGetUserAPI = async (token: string, auth0Id: object): Promise<any> => {
-	const params = {
-		method: "POST",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(auth0Id),
-	}
+	const params = getParams('POST', token, auth0Id)
 	const response = await fetch(`${basePath}/${apiVersion}/init-get-user`, params)
 	const data = await response.json()
 	return data
 }
 
 export const updateUserApi = async (userId: string, data: object, token: string): Promise<any> => {
-	const params = {
-		method: "PUT",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	}
+	const params = getParams('PUT', token, data)
 	const response = await fetch(`${basePath}/${apiVersion}/update-user/${userId}`, params)
 	const result = await response.json()
 	return result
 }
 
 export const changeLanguageAPI = async (data: object, token: string): Promise<any> => {
-	const params = {
-		method: "PUT",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	}
+	const params = getParams('PUT', token, data)
 	const response = await fetch(`${basePath}/${apiVersion}/change-language`, params)
 	const result = await response.json()
 	return result
 }
 
 export const updateWishlistColorAPI = async (data: object, token: string): Promise<any> => {
-	const params = {
-		method: "PUT",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	}
+	const params = getParams('PUT', token, data)
 	const response = await fetch(`${basePath}/${apiVersion}/update-wishlist-color`, params)
 	const result = await response.json()
 	return result
 }
 
 export const updateAppColorAPI = async (data: object, token: string): Promise<any> => {
-	const params = {
-		method: "PUT",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	}
+	const params = getParams('PUT', token, data)
 	const response = await fetch(`${basePath}/${apiVersion}/update-app-color`, params)
 	const result = await response.json()
 	return result
 }
 
 export const changeWishlistsDirectionApi = async (data: object, token: string): Promise<any> => {
-	const params = {
-		method: "PUT",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	}
+	const params = getParams('PUT', token, data)
 	const response = await fetch(`${basePath}/${apiVersion}/change-wishlists-direction`, params)
 	const result = await response.json()
 	return result
--- src/api/wishlist.api.ts
+++ src/api/wishlist.api.ts
@@ -1,108 +1,66 @@
 import { basePath, apiVersion } from "./utils/config";
 import { WishList } from "interfaces/wishlist";
+import getParams from "./utils/getParams";
 
 export const getWishlistsByUserIdApi = async (token: string): Promise<WishList> => {
-	const params = {
-		method: "GET",
-		headers: {
-			Authorization: `Bearer ${token}`
-		},
-	}
+	const params = getParams('GET', token, null)
 	const response = await fetch(`${basePath}/${apiVersion}/get-wishlists-by-user-id`, params)
 	const data = await response.json()
 	return data
 }
 
 export const postNewWishlistItemApi = async (wishlistId: string, item: object, token: string): Promise<WishList> => {
-	const params = {
-		method: "POST",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(item),
-	}
+	const params = getParams('POST', token, item)
 	const response = await fetch(`${basePath}/${apiVersion}/post-new-wishlist-item/${wishlistId}`, params)
 	const data = await response.json()
 	return data as WishList
 }
 
 export const postNewWishlistApi = async (data: object, token: string): Promise<WishList> => {
-	const params = {
-		method: "POST",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	}
+	const params = getParams('POST', token, data)
 	const response = await fetch(`${basePath}/${apiVersion}/post-new-wishlist`, params)
 	const result = await response.json()
 	return result as WishList
 }
 
 export const removeWishlistItemApi = async (wishlistId: string, wishlistItemId: string, token: any): Promise<WishList> => {
-	const params = {
-		method: "DELETE",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-	}
+	const params = getParams('DELETE', token, null)
 	const response = await fetch(`${basePath}/${apiVersion}/remove-wishlist-item/${wishlistId}/${wishlistItemId}`, params)
 	const data = await response.json()
 	return data
 }
 
 export const setCurrentWishlistAPI = async (wishlistId: string, userId: string, token: string): Promise<WishList> => {
-	const params = {
-		method: "GET",
-		headers: {
-			Authorization: `Bearer ${token}`
-		},
-	}
+	const params = getParams('GET', token, null)
 	const response = await fetch(`${basePath}/${apiVersion}/get-wishlist-by-id/${wishlistId}/${userId}`, params)
 	const data = await response.json()
 	return data
 }
 
 export const removeWishlistAPI = async (wishlistId: string, token: any): Promise<WishList> => {
-	const params = {
-		method: "DELETE",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-	}
+	const params = getParams('DELETE', token, null)
 	const response = await fetch(`${basePath}/${apiVersion}/remove-wishlist/${wishlistId}`, params)
 	const data = await response.json()
 	return data
 }
 
 export const updateWishlistApi = async (wishlistId: string, data: object, token: string): Promise<WishList> => {
-	const params = {
-		method: "PUT",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	}
+	const params = getParams('PUT', token, data)
 	const response = await fetch(`${basePath}/${apiVersion}/update-wishlist/${wishlistId}`, params)
 	const result = await response.json()
 	return result as WishList
 }
 
 export const updateWishlistItemApi = async (wishlistId: string, wishlistItemId: string, data: object, token: string): Promise<WishList> => {
-	const params = {
-		method: "PUT",
-		headers: {
-			Authorization: `Bearer ${token}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	}
+	const params = getParams('PUT', token, data)
 	const response = await fetch(`${basePath}/${apiVersion}/update-wishlist-item/${wishlistId}/${wishlistItemId}`, params)
+	const result = await response.json()
+	return result as WishList
+}
+
+export const changeWishlistColorApi = async (wishlistId: string, data: object, token: string): Promise<WishList> => {
+	const params = getParams('PUT', token, data)
+	const response = await fetch(`${basePath}/${apiVersion}/change-wishlist-color/${wishlistId}`, params)
 	const result = await response.json()
 	return result as WishList
 }
