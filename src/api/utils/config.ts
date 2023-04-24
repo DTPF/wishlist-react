5bef421fb0a218a0975332adf7793e88ad4ebdbc
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-04-24 14:10:12 +0200

    cahnge: backend port

--- src/api/utils/config.ts
+++ src/api/utils/config.ts
@@ -2,5 +2,5 @@ import { isLocalhost } from "utils/isLocalhost";
 
 export const apiVersion = 'v1'
 export const basePath = isLocalhost ?
-    `${window.location.protocol}//${window.location.hostname}:4003/api` :
+    `${window.location.protocol}//${window.location.hostname}:4000/api` :
     `${window.location.origin}/api`;
